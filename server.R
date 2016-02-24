09b6b4a40a0d45eb97c6c0f31f7f8bb6f73fb3cf
dtharvey <harvey@depauw.edu>
2016-02-24 12:44:59 -0500

    initial commit

---
+++ server.R
@@ -0,0 +1,59 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+
+load(file = "AbsTranPower.RData")
+
+output$transmittance = renderPlot({
+  plot(data$wavelength, data$trans, type = "l", lwd = 3, col = "blue",
+       xlab = "wavelength (nm)", ylab = "transmittance", 
+       main = paste0("Transmission = ", trans()), ylim = c(0, 1))
+  abline(v = data$wavelength[input$wavelength-399], lwd =3, col = "red")
+})
+
+output$power_sample = renderPlot({
+  plot(data$wavelength, data$p_sam, type = "l", lwd = 3, col = "blue",
+       xlab = "wavelength (nm)", ylab = expression(italic(P)[sample]), 
+       main = paste0("Sample Power = ", psam()), ylim = c(0, 1))
+  abline(v = data$wavelength[input$wavelength-399], lwd =3, col = "red")
+})
+
+output$absorbance = renderPlot({
+  plot(data$wavelength, data$abs, type = "l", lwd = 3, col = "blue",
+       xlab = "wavelength (nm)", ylab = "absorbance", 
+       main = paste0("Absorbance = ", abs()), ylim = c(0, 1))
+  abline(v = data$wavelength[input$wavelength-399], lwd =3, col = "red")
+})
+
+output$power_reference = renderPlot({
+  plot(data$wavelength, data$p_ref, type = "l", lwd = 3, col = "blue",
+       xlab = "wavelength (nm)", ylab = expression(italic(P)[reference]), 
+       main = paste0("Reference Power = ", pref()), ylim = c(0, 1))
+  abline(v = data$wavelength[input$wavelength-399], lwd = 3, col = "red")
+})
+
+abs = reactive({
+  noquote(sprintf("%.4f", data$abs[input$wavelength-399]))
+})
+
+trans = reactive({
+  noquote(sprintf("%.4f", data$trans[input$wavelength-399]))
+})
+
+pref = reactive({
+  noquote(sprintf("%.4f", data$p_ref[input$wavelength-399]))
+})
+
+psam = reactive({
+  noquote(sprintf("%.4f", data$p_sam[input$wavelength-399]))
+})
+
+# output$test = renderText({
+#     paste0(abs(), " = -log(", trans(), ") = -log(", psam(), ")/(", pref(), ")")
+# })
+
+output$equation = renderText({
+  paste0(abs(), " = \u2013log(", trans(), ") = \u2013log{(", psam(), ")/(", pref(), ")}")
+})
+
+})
---
+++ ui.R
@@ -0,0 +1,88 @@
+library(shiny)
+
+# initial set up
+
+shinyUI(fluidPage(
+  withMathJax(),
+  tags$style(HTML("
+                  h1{
+                    font-size: 48px;
+                  }
+                 ")),
+  
+  fluidRow(column(
+      width = 3,
+      br(),
+      HTML("<h3><center>Beer's Law</center></h3>"),
+      HTML("<p>Demonstration of the relationship between the source's 
+           power as passed by a reference, \\({P}_{\\textrm{reference}}\\),
+           the source's power as passed by the sample, 
+           \\({P}_{\\textrm{sample}}\\), the transmittance of the source's
+           radiation by the sample, <em>T</em>, and the absorbance of 
+           the source's radiation, <em>A</em>, by the sample.</p>")
+      # HTML("<p>\\(A = -\\textrm{log}T =
+      #      -\\textrm{log}\\frac { { P }_{ \\textrm{sample} }}
+      #      { { P }_{\\textrm{reference}} }\\)</p>")
+    ),
+    column(
+      width = 3,
+      plotOutput("transmittance", height = "300px", width = "350px")
+    ),
+    column(
+      width = 3,
+      plotOutput("power_sample", height = "300px", width = "350px")
+    )
+  ),
+  fluidRow(
+    column(
+      width = 9,
+      offset = 2,
+      align = "center",
+      # HTML("<h1>\\(A = -\\textrm{log}T =
+      #      -\\textrm{log}\\frac { { P }_{ \\textrm{sample} }}
+      #      { { P }_{\\textrm{reference}} }\\)</h1>"
+      #      ),
+      HTML("<h1>\\(A = -\\textrm{log}T =
+            -\\textrm{log}\\left\\{ {P}_{\\textrm{sample}}/
+            {P}_{\\textrm{reference}} \\right\\}\\)</h1>"
+           ),
+      h1(textOutput("equation"))
+    )
+  ),
+  fluidRow(
+    column(
+      width = 3,
+      offset = 1,
+      plotOutput("absorbance", height = "300px", width = "350px")
+    ),
+    column(
+      width = 3,
+      offset = 1,
+      align = "left",
+      br(),
+      sliderInput("wavelength", "Wavelength?", min = 400, max = 750, 
+                  step = 1, value = 400,
+                  animate = 
+                    animationOptions(interval = 500, 
+                                     playButton = "Run",
+                                     pauseButton = "Stop")),
+
+      HTML("<p>Adjust the slider to change the wavelength and view the
+            relationship between the four values. Click on the  
+            run button to advance through the wavelengths automatically 
+            (note: the animation advances at a rate of 2 nm/sec, so you 
+            first may wish to adjust the wavelength to a region of 
+            interest).</p>")
+    ),
+    column(
+      width = 3,
+      offset = 1,
+      align = "left",
+      plotOutput("power_reference", height = "300px", width = "350px")
+    )
+  )
+    
+
+  
+# end of shiny ui
+))
